f05194dd038b15e612bdd98376e993d23f68d7de
ikiv-rach <victoriasalmi18@gmail.com>
2020-03-26 01:02:44 +0100

    initial commit

---
+++ Js Task/Task 1.js
@@ -0,0 +1,16 @@
+let name="Victoria Salami";
+let courses = ["HTML","CSS","JavaScript","Design"];
+console.log("My name is: " + name);
+console.log("The courses i offer includes:"+courses);
+const data = [];
+for (let v=1; v<=200; v++) {
+
+    if (v%2===0) {
+ 
+      data.push(v);
+ 
+    }
+ 
+  }
+ 
+  console.log(data);
